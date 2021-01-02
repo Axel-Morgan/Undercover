ddf5d5f6cfd6f996c6eebfb45f24c2099ee348c5
Axel-Morgan <61938702+Axel-Morgan@users.noreply.github.com>
2021-01-02 23:20:46 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,102 +1,162 @@
-/*------------------ЛАМПОЧКА И ОСВЕЩЕНИЕ ТЕКСТА------------------*/
-var objectLamp = {
-    сursorPosition: 0,
-    correction: 1.8,
-    //Свойства лампы
-    lamp: document.querySelector('.lamp_Js'),
-    lampJob: false,
-    lampContainer: document.querySelector('.lampContainer_Js'),
-    //Свойства строки со слоганом
-    sloganWidth: document.querySelector('.slogan').offsetWidth,
-    arrayWordSlogan: document.querySelectorAll('.header__lamp-mechanism span'),
-    //Цветовые свойства
-    borderSaturate: 6.1,
-    borderLightness: 25.9
+/*----------------------------ЛАМПОЧКА И ОСВЕЩЕНИЕ ТЕКСТА----------------------------*/
+//Параметры курсора
+var cursorLamp = {
+    xPosition: 0,
+    isJammed: false
 };
 
-//КОРРЕКТИРОВКА
+//То, что имеет отношение к контейнеру с лампой 
+var lampMechanism = {
+    isFlashes: false,
+    lampRestrictionMovement: 2.5,
+    lampCollider: document.querySelector('.lampCollider_Js'),
+    lampContainer: document.querySelector('.lampContainer_Js'),
+    lampImage: document.querySelector('.lamp')
+};
+
+//То, что имеет отношение к котейнеру с текстом
+var underLampText = {
+    sloganContainer: document.querySelector('.slogan'),
+    sloganLetters: document.querySelectorAll('.slogan span'),
+    hue: 39,
+    saturate: 30.8,
+    loverBoundSaturate: 6.1,
+    lightness: 55.1,
+    loverBoundLightness: 25.9
+};
+
+//Объекты Window 
 window.onload = function(){
-    changeLetters(0);
+    changeColorLetters(0);
 }
 
 window.onresize= function(){
-    objectLamp.sloganWidth = document.querySelector('.slogan').offsetWidth;
+    setTimeout(changeColorLetters, 300, 0);
 }
 
-//ФУНКЦИЯ ДВИЖЕНИЯ ЛАМПОЧКИ 
-function moveLamp(job, x){
-    let сursorPosition = objectLamp.сursorPosition,
-        lampContainer = objectLamp.lampContainer,
-        shift = 0;
+//-----------------------Двигатель лампы 
+function moveLampContainer(x, wSC){
+    let lRM = lampMechanism.lampRestrictionMovement,
+        lCont = lampMechanism.lampContainer,
+        xStart = cursorLamp.xPosition;
+    
+    let shift = xStart - x;
 
-    if (job == true){
-        shift = сursorPosition - x;
-        if ((shift > -objectLamp.sloganWidth/2.5) && (shift < objectLamp.sloganWidth/2.5)){
-            lampContainer.style.right = сursorPosition - x + 'px';
+    //Проверяем, достигла ли лампочка границ области
+    if ((shift > -wSC/lRM) && (shift < wSC/lRM)){
+        lCont.style.right = shift + 'px';
+
+        if ((shift < -wSC/lRM * 0.8) || (shift > wSC/lRM * 0.8)){
+            lampFlashes(true); //Лампа мигает 
         }
-
-        else{
-            lampContainer.style.right = 0 + 'px';
+        else{ 
+            lampFlashes(false); // Лампа не мигает
         }
     }
     else{
-        lampContainer.style.right = 0 + 'px';
+        lampFlashes(false); // Лампа не мигает
+        returnLampOnStart(lCont); //Возвращаем на место лампу
     }
-    changeLetters(shift);
+
+    //Вызываем функцию изменения цвета букв
+    changeColorLetters(shift);
 }
 
-//МЕНЯЕМ ЦВЕТ БУКВ
-function changeLetters(shift){
-    let letters = objectLamp.arrayWordSlogan,
-        hue = 39, 
-        saturate = 30.8, 
-        lightness = 55.1,
+//-----------------------Заставляем лампу мигать при приближении к границе
+function lampFlashes(boolFlashes, speedAnimation = 0.7){
+    let lImg = lampMechanism.lampImage;
+
+    if (boolFlashes == false){
+        speedAnimation = 0;
+    }
+
+    lImg.style.animation = 'lamp-flashes ' + speedAnimation +'s infinite ease-in-out';
+} 
+
+//-----------------------Возвращаем лампу на место 
+function returnLampOnStart(obj){
+    obj.style.right = 0 + 'px';
+    changeColorLetters(0);
+}
+
+//-----------------------Подсвечиваем текст в зависимости от положения текста
+function changeColorLetters(shift, hue = underLampText.hue, saturate = underLampText.saturate, lightness = underLampText.lightness){
+    let nLet = underLampText.sloganLetters,
+        widthContainerLetters = underLampText.sloganContainer.offsetWidth,
+        fontSize = parseInt(window.getComputedStyle(underLampText.sloganLetters[0], null).getPropertyValue('font-size'), 10),
+        h = hue, s = saturate, l = lightness,
+        borderS = underLampText.loverBoundSaturate, borderL = underLampText.loverBoundLightness,
+        coefficient = 0,
         startPoint = 0;
     
-    console.log(letters.length)
-    startPoint = ((objectLamp.sloganWidth/letters.length) * (letters.length/2)) - shift;
-    
-    startPoint = parseInt((startPoint/letters.length) * objectLamp.correction);
-
-    let h = hue, s = saturate, l = lightness;
+    coefficient = 100/fontSize * 0.8;
+    startPoint = ((widthContainerLetters/nLet.length) * (nLet.length/2)) - shift;
+    startPoint = parseInt(startPoint/nLet.length * coefficient);    
 
     for (let i = startPoint; i >= 0; i--){
-        s = s - (startPoint - i - 0.4);
-        if (s < objectLamp.borderSaturate) s = objectLamp.borderSaturate;
+        s = s - (startPoint - i);
+        if (s < borderS) s = borderS;
         l = l - (startPoint - i);
-        if (l < objectLamp.borderLightness) l = objectLamp.borderLightness;
-        letters[i].style.color = 'hsl(' + h + ', ' + s + '%, '+ l +'%)';
+        if (l < borderL) l = borderL;
+        nLet[i].style.color = 'hsl(' + h + ', ' + s + '%, '+ l +'%)';
     }
 
    h = hue; s = saturate; l = lightness;
 
-    for (let i = startPoint; i < letters.length; i++){
+    for (let i = startPoint; i < nLet.length; i++){
         s = s + (startPoint - i);
-        if (s < objectLamp.borderSaturate) s = objectLamp.borderSaturate;
+        if (s < borderS) s = borderS;
         l = l + (startPoint - i);
-        if (l < objectLamp.borderLightness) l = objectLamp.borderLightness;
-        letters[i].style.color = 'hsl(' + h + ', ' + s + '%, '+ l +'%)';
+        if (l < borderL) l = borderL;
+        nLet[i].style.color = 'hsl(' + h + ', ' + s + '%, '+ l +'%)';
     }
 }
 
-//При зажатии ПКМ на лампочке 
-objectLamp.lamp.addEventListener("mousedown", function(){
-    objectLamp.lampJob = true;
-    objectLamp.сursorPosition = event.clientX;
+//-----------------------EVENT LISTENER-----------------------
+//-----------------------Зажата ЛКМ на коллайдере лампы
+function forLampMouseDown(){
+    event.preventDefault();
+    cursorLamp.xPosition = event.clientX; 
+    cursorLamp.isJammed = true;
+}
+
+lampMechanism.lampCollider.addEventListener("mousedown", function(){ 
+    forLampMouseDown(); //Мышка
 });
 
-//При отпускании ПКМ на лампочке 
-document.addEventListener("mouseup", function(){
-    objectLamp.lampJob = false;
+lampMechanism.lampCollider.addEventListener("touchstart", function(){ 
+    forLampMouseDown(); //Тачскрин 
 });
 
-//Выход курсора за область видимости 
-objectLamp.lamp.addEventListener("mouseleave", function(){
-    objectLamp.lampContainer.style.right = 0 + 'px';
-    changeLetters(0);
+//-----------------------ЛКМ отжата 
+function forLampMouseUp(){
+    event.preventDefault();
+    cursorLamp.isJammed = false;
+    lampFlashes(false);
+    changeColorLetters(0);
+    returnLampOnStart(lampMechanism.lampContainer);
+}
+
+document.addEventListener("mouseup", function(){ 
+    forLampMouseUp(); //Мышка
 });
 
-//Мыша двигается
-objectLamp.lamp.addEventListener("mousemove", function(){
-    moveLamp(objectLamp.lampJob, event.clientX);
+document.addEventListener("touchend", function(){ 
+    forLampMouseUp(); //Тачскрин 
 });
+
+//-----------------------Курсор двигается с зажатой ЛКМ
+function forLampMouseMove(){
+    if (cursorLamp.isJammed == true){ 
+        moveLampContainer(event.clientX, underLampText.sloganContainer.offsetWidth);
+    }
+}
+
+lampMechanism.lampCollider.addEventListener("mousemove", function(){ 
+    forLampMouseMove(); //Мышка
+});
+
+lampMechanism.lampCollider.addEventListener("touchmove", function(){ 
+    forLampMouseMove(); //Тачскрин 
+});
+
