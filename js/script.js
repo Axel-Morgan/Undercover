42fe011bcfdfd2d39468aaf1c48f018617d6f280
Axel-Morgan <61938702+Axel-Morgan@users.noreply.github.com>
2021-01-26 17:02:16 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,11 +16,108 @@ window.onresize= function(){
     setTimeout(changeColorLetters, 300, 0); //При изменение разрешения экрана, функции необходимо время, чтобы понять истинное значение ширины экрана
 
     //Скрипты, необходимые для файла showServicesArticle
-    InitializingNavServices();
+    setTimeout(InitializingNavServices, 200);
 }
+/*-------------------------------------УПРАВЛЕНИЕ МЕНЮ-------------------------------------*/
+//Кнопки показа
+const menuButtons = {
+    mainButton: document.querySelector('.menu__button-open'),
+    signButtonClose: document.querySelector('.sign__container > .button-close__container'),
+};
+
+//Все, что имеет отношение к ведущему меню
+const mainMenu = {
+    header: document.querySelector('header'),
+    sections: document.querySelectorAll('section'),
+    menuContainer: document.querySelector('.main-menu__container'),
+    links: document.querySelector('.main-menu__links-container'),
+    isShow: false
+};
+
+//Все, что имеет отношение к окну входа
+const signIn = {
+    signInContainer: document.querySelector('.sign-in')
+};
+
+//Все, что имеет отношение к окну регистрации
+const signUp = {
+    signUpContainer: document.querySelector('.sign-up'),
+};
+
+function controlUnitMenu(val){
+    let link = val.innerHTML;
+
+    switch (link){
+        case 'sign in': toOpenSign(0); break;
+        case 'sign up': toOpenSign(1); break;
+        case 'main': toMoveToThereFromMain(0); break;
+        case 'services': toMoveToThereFromMain(1); break;
+        case 'portfolio': toMoveToThereFromMain(2); break;
+        case 'feedbacks': toMoveToThereFromMain(3); break;
+        case 'contact us': toMoveToThereFromMain(4); break;
+        case 'footer': toMoveToThereFromMain(5); break;
+    }
+}
+
+//Показ главного меню
+function toShowMenu(isShow){
+    let menu = mainMenu.menuContainer;
+
+    if (!isShow){
+        menu.style.display = "flex";
+        mainMenu.isShow = true;
+        menuButtons.mainButton.classList.add('itIsPush');
+    }
+    else{
+        menu.style.display = "";
+        mainMenu.isShow = false;
+        toCloseSign();
+        menuButtons.mainButton.classList.remove('itIsPush');
+    }
+}
+
+//Показ Sign котнейнера 
+function toOpenSign(val){
+   let sign = (val == 0) ? signIn.signInContainer : signUp.signUpContainer;
+
+   sign.style.display = "flex";
+}
+
+//Закрытие Sign окон
+function toCloseSign(){
+    signIn.signInContainer.style.display = "";
+    signUp.signUpContainer.style.display = "";
+}
+
+//Перемещаем скрол на необходимую секцию после выбора соответствующей ссылки
+function toMoveToThereFromMain(val){
+    if (val != 0 && val != 5){
+        let section = mainMenu.sections[val - 1];
+
+        section.scrollIntoView(true);
+        toShowMenu(true);
+        toCloseSign()
+    }
+
+    else if (val == 0){
+        let header = mainMenu.header;
+
+        header.scrollIntoView(true);
+        toShowMenu(true);
+        toCloseSign()
+    }
+}
+
+menuButtons.mainButton.addEventListener("click", function(){
+    toShowMenu(mainMenu.isShow);
+});
+
+mainMenu.links.addEventListener("click", function (e) {
+    controlUnitMenu(e.target);
+});
 /*-------------------------------------БЛОК УПРАВЛЕНИЯ СЕКЦИЕЙ PORTFOLIO-------------------------------------*/
 //Все, что имеет отношение к списку категорий 
-var portfolioSelecting = {
+const portfolioSelecting = {
     selectingContainer: document.querySelector('.selecting_category__container'),
     currentSelection: document.querySelector('.selecting_category__window > .text > p'),
     selectingWindow: document.querySelector('.selecting_category__window'),
@@ -29,7 +126,7 @@ var portfolioSelecting = {
 };
 
 //Все что имеет отношение к статьям
-var portfolioArticles = {
+const portfolioArticles = {
     maxShowArticles: 4,
     collection: [],
     articles: document.querySelectorAll('.articles_portfolio__item'),
@@ -37,7 +134,7 @@ var portfolioArticles = {
 };
 
 //Все что относится к блоку управления статьями PREV и NEXT
-var portfolioControl = {
+const portfolioControl = {
     pageNumber: 1,
     last: 1,
     currently: portfolioArticles.maxShowArticles,
@@ -99,8 +196,6 @@ function toChooseCategory(val){
    toOpenSelecting(portfolioSelecting.isSelect); 
    toAddCollectionPortfolio(nameCategory);
 }
-
-//HOVER ДЛЯ СТАТЕЙ
 
 //СОЗДАЕМ КОЛЛЕКЦИЮ СТАТЕЙ, КОТОРЫЕ НЕОБХОДИМО ПОКАЗАТЬ 
 function toAddCollectionPortfolio(name){
@@ -231,13 +326,13 @@ portfolioSelecting.selectingWindow.addEventListener("mouseout", function(){
 });
 /*----------------------------НАВИГАЦИОННЫЙ БЛОК РАЗДЕЛА SERVICES----------------------------*/
 //Экран 
-var yourWindow = {
+const yourWindow = {
     width: window.outerWidth,
     firstInit: true
 }
 
 //Все, что относится к навигационному блоку
-var navigationServices = {
+const navigationServices = {
     links: document.querySelectorAll('.nav_services_item > p'),
     linksStartHeight: 492,
     linkContainerHeight: document.querySelector('.navigation_services_container').offsetHeight,
@@ -251,7 +346,7 @@ var navigationServices = {
 };
 
 //Все, что относится к статьям 
-var ariclesServices = {
+const ariclesServices = {
     articlesContainer: document.querySelectorAll('.articles_services__item'),
     articlesTextContainer: document.querySelectorAll('.articles_services__item > p'),
     articlesImages: document.querySelectorAll('.background_aricles_services')
@@ -386,13 +481,13 @@ function toMoveImageServices(val){
 
 /*----------------------------ЛАМПОЧКА И ОСВЕЩЕНИЕ ТЕКСТА----------------------------*/
 //Параметры курсора
-var cursorLamp = {
+const cursorLamp = {
     xPosition: 0,           //Запоминаем позицию курсора
     isJammed: false         //Зажата ли клавиша? 
 };
 
 //То, что имеет отношение к контейнеру с лампой 
-var lampMechanism = {
+const lampMechanism = {
     isFlashes: false,                                           //Должна ли лампа мигать?
     lampRestrictionMovement: 2.5,                               //Граница движения лампы
     lampCollider: document.querySelector('.lampCollider_Js'),   //Коллайдер лампы, активная область
@@ -404,7 +499,7 @@ var lampMechanism = {
 };
 
 //То, что имеет отношение к котейнеру с текстом
-var underLampText = {
+const underLampText = {
     sloganContainer: document.querySelector('.slogan'),         //Контейнер со всем текстом
     sloganLetters: document.querySelectorAll('.slogan span'),   //Массив символов
     hue: 39,                                                    //Hue (Hsl)
